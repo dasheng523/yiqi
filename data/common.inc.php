262f75a0c5ca16cd0d5ec7b381e50f7df3edbae4
dasheng523 <dasheng523@163.com>
2016-05-14 15:38:59 +0800

    first commit

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'gzylyq';
+$cfg_dbuser = 'gzylyq';
+$cfg_dbpwd = 'gzylyq2016';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
